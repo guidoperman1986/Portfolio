d284173958b12a149da6ad82d7e208e5176809aa
Guido <guidoperman@gmail.com>
2018-04-25 13:28:30 -0300

    componentes de pantalla listos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { PortafolioComponent } from './components/portafolio/portafolio.componen
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    PortafolioComponent
+    PortafolioComponent,
+    AboutComponent,
+    PortafolioItemComponent
   ],
   imports: [
     BrowserModule
