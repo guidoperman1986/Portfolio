f6bf62669cf887a3095dc33412d7ec0c093977e4
Guido <guidoperman@gmail.com>
2018-05-02 15:14:39 -0300

    Servicio de Firebase listo para mostrar

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,12 +5,28 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  equipo:any[] = [];
+  cargada:boolean=false;
+  cargada_sobre_nosotros:boolean=false;
 
   constructor(public http:Http) {
-      this.http.get("assets/data/info.pagina.json")
-      .subscribe( data =>{console.log(data.json())
-        //this.cargada = true;
-        this.info=data.json();
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
+
+  public carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+    .subscribe( data =>{
+      this.cargada = true;
+      this.info=data.json();
+    })
+  }
+
+  public carga_sobre_nosotros(){
+      this.http.get("https://portafolio-19d72.firebaseio.com/equipo.json")
+      .subscribe( data =>{console.log(data.json());
+        this.cargada_sobre_nosotros = true;
+        this.equipo=data.json();
       })
   }
 
