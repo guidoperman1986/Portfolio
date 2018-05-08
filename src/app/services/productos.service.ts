98a0fcc45d337b042420eb7f2a1bdf5a84f537bb
Guido <guidoperman@gmail.com>
2018-05-08 13:41:20 -0300

    Se cargo el portafolio con datos provenientes de firebase

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,17 +4,19 @@ import { Http } from "@angular/http";
 @Injectable()
 export class ProductosService {
   productos:any[]=[];
-
+  cargando:boolean=true;
 
   constructor(private http:Http) {
     this.cargar_productos();
   }
 
   public cargar_productos(){
-
+        this.cargando=true;
         this.http.get('https://portafolio-19d72.firebaseio.com/productos_idx.json')
             .subscribe( res=>{
               console.log(res.json());
+              this.cargando=false;
+              this.productos=res.json();
             });
 
 
