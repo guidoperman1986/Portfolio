549c15c8bee6c31b24cd83976d8085e3b1ffc684
Guido <guidoperman@gmail.com>
2018-05-18 12:38:39 -0300

    pagina terminada

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,10 +4,37 @@ import { Http } from "@angular/http";
 @Injectable()
 export class ProductosService {
   productos:any[]=[];
+  productos_filtrado:any[]=[];
   cargando:boolean=true;
 
   constructor(private http:Http) {
     this.cargar_productos();
+  }
+
+  public buscar_producto(termino:string){
+      console.log(this.productos.length);
+
+      if (this.productos.length){
+        this.cargar_productos().then( ()=>{
+          this.filtrar_productos(termino);
+        });
+      }else{
+          this.filtrar_productos(termino);
+      }
+  }
+
+  private filtrar_productos(termino:string){
+      this.productos_filtrado = [];
+      termino = termino.toLowerCase();
+
+
+      this.productos.forEach(prod=>{
+        if (prod.categoria.indexOf(termino)>= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0){
+          this.productos_filtrado.push(prod);
+        //  console.log(prod);
+        }
+        //console.log(prod);
+      })
   }
 
   public cargar_producto(cod:string){
@@ -16,14 +43,17 @@ export class ProductosService {
 
   public cargar_productos(){
         this.cargando=true;
-        this.http.get('https://portafolio-19d72.firebaseio.com/productos_idx.json')
+
+        let promesa = new Promise((resolve,reject)=>{
+          this.http.get('https://portafolio-19d72.firebaseio.com/productos_idx.json')
             .subscribe( res=>{
-              console.log(res.json());
+              //console.log(res.json());
               this.cargando=false;
               this.productos=res.json();
+              resolve();
             });
-
-
+        });
+        return promesa;
   }
 
 }
